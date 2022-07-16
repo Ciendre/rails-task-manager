951b12f6a2157a654f490a657a32530449537fc3
nicolas nguon <nicolas.nguon@checkmyguest.fr>
2022-07-16 11:14:43 +0200

    gems modif

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #Inventer le routing CRUD pour l'exerice
+
+  #Lire toutes les taches
+  get "tasks" => "tasks#index"
+
+  #Lire une tâche
+  get "tasks/:id" => "tasks#show"
+
+  #ajouter une nouvelle tâche
+  get "tasks/new" => "tasks#new"
+  post "tasks" => "tasks#create"
+
+  #modifier une tache
+  get "tasks/:id/edit" => "tasks#edit"
+  patch "tasks/:id" => "tasks#update"
+
+  #supprimer une tache
+  delete "tasks/:id" => "tasks#destroy"
 end
